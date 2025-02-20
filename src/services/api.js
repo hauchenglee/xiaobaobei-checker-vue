a68bd6e927f49e6369f769d44b3ccbdc16b85086
“hauchenglee” <hauchenglee@gmail.com>
2025-02-20 16:10:41 +0800

    feat(api): to docker name

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:5000'
+const API_BASE_URL = 'http://xiaobaobei-checker:5000'
 
 export const checkText = async (text, terms = [], context = false) => {
     try {
