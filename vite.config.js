6def13703e5b863a1fa9a779459a9ef5f1864459
“hauchenglee” <hauchenglee@gmail.com>
2025-02-20 17:19:57 +0800

    feat(config): update config hmr and headers

--- vite.config.js
+++ vite.config.js
@@ -19,10 +19,21 @@ export default defineConfig({
     host: '0.0.0.0',    // 允許外部訪問
     port: 5173,         // 指定端口
     strictPort: true,   // 如果端口被占用，直接報錯
+    cors: true,
     allowedHosts: [
       'srv415056.hstgr.cloud',
       'localhost',
       '62.72.59.32'
-    ]
+    ],
+    hmr: {
+      host: 'srv415056.hstgr.cloud',
+      port: 5173,
+      protocol: 'http'
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    }
   }
 })
