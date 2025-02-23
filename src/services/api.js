8588c3130a5f7d259c95fffba75278cf04574876
hauchenglee <hauchenglee@gmail.com>
2025-02-23 22:23:16 +0800

    feat(check): add isAi and terms

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = 'http://srv415056.hstgr.cloud:5001'
 
-export const checkText = async (text, terms = [], context = false) => {
+export const checkText = async (text, terms = [], is_ai = false) => {
     try {
         const response = await fetch(`${API_BASE_URL}/check`, {
             method: 'POST',
@@ -10,7 +10,7 @@ export const checkText = async (text, terms = [], context = false) => {
             body: JSON.stringify({
                 article: text,
                 terms,
-                context
+                is_ai
             })
         });
 
